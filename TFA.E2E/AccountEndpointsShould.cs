203ccc33b8f921ace9a4a5e7fc27ccf8fa252c7c
kirill <kirill220798@mail.ru>
2025-01-04 00:28:51 +0700

    Store user password and andpoint for signin & signon

---
+++ TFA.E2E/AccountEndpointsShould.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using System.Net.Http.Json;
+using TFA.Domain.Authentication;
+
+namespace TFA.E2E;
+
+public class AccountEndpointsShould : IClassFixture<ForumApiApplicationFactory>
+{
+    private readonly ForumApiApplicationFactory factory;
+
+    public AccountEndpointsShould(ForumApiApplicationFactory factory)
+    {
+        this.factory = factory;
+    }
+
+    [Fact]
+    public async Task SignInAfterSignOn()
+    {
+        using var httpClien = factory.CreateClient();
+        using var response = await httpClien.PostAsync("account", JsonContent.Create(new { login = "TestLogin", password = "Qwerty123" }));
+        response.Invoking(r => r.EnsureSuccessStatusCode()).Should().NotThrow();
+        var createdUser = await response.Content.ReadFromJsonAsync<User>();
+        using var signInResponse = await httpClien.PostAsync("account/signin", JsonContent.Create(new { login = "TestLogin", password = "Qwerty123" }));
+        signInResponse.IsSuccessStatusCode.Should().BeTrue();
+        signInResponse.Headers.Should().ContainKey("TFA-Auth-Token");
+        var signedInUser = await signInResponse.Content.ReadFromJsonAsync<User>();
+        createdUser.Should().NotBeNull();
+        signedInUser
+            .Should()
+            .NotBeNull()
+            .And
+            .Subject.As<User>().UserId
+            .Equals(createdUser!.UserId);
+    }
+}
---
+++ TFA.E2E/TopicEndpointsShould.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace TFA.E2E;
+
+public class TopicEndpointsShould : IClassFixture<ForumApiApplicationFactory>, IAsyncLifetime
+{
+    private readonly ForumApiApplicationFactory factory;
+    private readonly Guid forumId = Guid.Parse("92f9a622-ca5e-46d5-8569-c09d6c914faa");
+    public TopicEndpointsShould(ForumApiApplicationFactory factory)
+    {
+        this.factory = factory;
+    }
+    public async Task InitializeAsync()
+    {
+        //var dbContext = factory.Services.GetRequiredService<ForumDbContext>();
+        //var cretedForum = await dbContext.Forums.AddAsync(
+        //    new Forum()
+        //    {
+        //        ForumId = forumId,
+        //        Title = "Test forum"
+        //    }
+        //    );
+        //await dbContext.SaveChangesAsync();
+    }
+
+    [Fact]
+    public async Task ReturnForbidden_WhenNotAutenticated()
+    {
+        using var httpClient = factory.CreateClient();
+        using var forumCreatedResponse = await httpClient.PostAsync("forums", JsonContent.Create(new { Title = "Test forum" }));
+        forumCreatedResponse.EnsureSuccessStatusCode();
+        var createdForum = await forumCreatedResponse.Content.ReadFromJsonAsync<API.Models.Forum>();
+        createdForum.Should().NotBeNull();
+
+        using var response = await httpClient.PostAsync($"forums/{createdForum!.Id}/topics", 
+            JsonContent.Create(new { Title = "Test topic name" }));
+        response.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+    }
+
+    public Task DisposeAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+}
